0e8ecb96fb9ce4a4ede4fbb951d620f62ea12cf8
santopotter <gustavo.geldes@alumnos.uv.cl>
2020-03-30 12:34:28 -0300

    Updates

---
+++ precache-manifest.74bd4d2fe68089900b3e4118572bf2f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb36138c214bdcb804bfa4b837329e6",
+    "url": "/paginas/./index.html"
+  },
+  {
+    "revision": "7273431c6f58758de43e",
+    "url": "/paginas/./static/css/main.f37e8fd3.chunk.css"
+  },
+  {
+    "revision": "bedb126fb7dadcad6ce2",
+    "url": "/paginas/./static/js/2.72e89e63.chunk.js"
+  },
+  {
+    "revision": "7273431c6f58758de43e",
+    "url": "/paginas/./static/js/main.010a30e3.chunk.js"
+  },
+  {
+    "revision": "b701dc00c8fb54da0703",
+    "url": "/paginas/./static/js/runtime-main.7d13c682.js"
+  }
+]);
